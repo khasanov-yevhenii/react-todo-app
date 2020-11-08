4777defa2a44ac29d26a30728a7e6fab6980d287
Khasanov <zkhasanovv@gmail.com>
2020-11-08 21:21:46 +0200

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+import listSvg from './assets/images/list.svg';
+import greenSvg from './assets/images/green.svg';
+
 
 function App() {
     return (
-        <div className="App">
-            <h1>todo</h1>
+        <div className="todo">
+            <div className="todo__sidebar">
+                <Sidebar items={[
+                    {
+                        icon: listSvg,
+                        title: 'All tasks',
+                        active: false
+                    }
+                ]}/>
+                <Sidebar items={[
+                    {
+                        icon: greenSvg,
+                        title: 'Lessons',
+                        active: true
+                    },
+                    {
+                        icon: greenSvg,
+                        title: 'Game',
+                        active: true
+                    },
+                    {
+                        icon: greenSvg,
+                        title: 'Work',
+                        active: false
+                    }
+                ]}/>
+            </div>
+            <div className="todo__main">
+                m
+            </div>
         </div>
     );
 }
